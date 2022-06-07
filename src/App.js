b25b7f5762a9ed93defa611779d77545be6419bd
alexbellairs <alexanderbellairs@gmail.com>
2022-06-07 11:45:08 +0100

    fourth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 import ImgLink from "./components/ImgLink";
 import Job from "./components/Job";
 import Person from "./components/Person";
 import Pet from "./components/Pet";
-// import WinxSaga from "./images/winx-saga.jpg";
-// import OTW from "./images/outside-the-wire.jpg";
-// import STdisco from "./images/startrek-discovery.jpg";
-// import QueenG from "./images/queens-gambit.jpg";
-// import Witcher from "./images/witcher.jpg";
+import WinxSaga from "./images/winx-saga.jpg";
+import OTW from "./images/outside-the-wire.jpg";
+import STdisco from "./images/startrek-discovery.jpg";
+import QueenG from "./images/queens-gambit.jpg";
+import Witcher from "./images/witcher.jpg";
 import ImageLink from "./components/ImageLink";
 import Float from "./images/float.jpg";
 import LivingRoom from "./images/lux-living-room.webp";
@@ -15,6 +16,16 @@ import Villa from "./images/villa.jpg";
 import Snow from "./images/winter.jpg";
 
 const App = () => {
+  const [persons, setPerson] = useState([
+    { name: "Dave", age: 33 },
+    { name: "Karen", age: 45 },
+    { name: "Steve", age: 40 },
+  ]);
+  const handleClick = (index) => {
+    let storedPeople = [...persons];
+    storedPeople.splice(index, 1);
+    setPerson(storedPeople);
+  };
   return (
     <div className="App">
       <h1>Codenation Week 5</h1>
@@ -22,11 +33,13 @@ const App = () => {
       <Job job="Junior Developer" />
       <Pet petsName="Cooper" type="Dog" />
       <div className="images">
-        {/* <ImageLink file={WinxSaga} alt="Winx Saga Poster" />
+        <ImageLink file={WinxSaga} alt="Winx Saga Poster" />
         <ImageLink file={OTW} alt="Outside The Wire Poster" />
         <ImageLink file={STdisco} alt="Star Trek Discovery Poster" />
         <ImageLink file={QueenG} alt="Queens Gambit Poster" />
-        <ImageLink file={Witcher} alt="The Witcher Poster" /> */}
+        <ImageLink file={Witcher} alt="The Witcher Poster" />
+      </div>
+      <div className="images">
         <ImgLink
           file={LivingRoom}
           alt="Luxury Living Room"
@@ -36,6 +49,17 @@ const App = () => {
         <ImgLink file={Float} alt="Floating Villa" para="Unique Stays" />
         <ImgLink file={Snow} alt="Winter Cabin" para="Pets Allowed" />
       </div>
+      {persons.map((person, index) => {
+        return (
+          <Person
+            name={person.name}
+            age={person.age}
+            key={index}
+            handleClick={handleClick}
+          />
+        );
+      })}
+      ;
     </div>
   );
 };
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,9 +1,10 @@
-const Person = ({ name, age }) => {
+const Person = ({ name, age, handleClick }) => {
   return (
     <div>
       <h1>
         My name is {name} and I am {age} years old{" "}
       </h1>
+      <button onClick={handleClick}>Remove</button>
     </div>
   );
 };
